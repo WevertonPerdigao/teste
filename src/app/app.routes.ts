f8067b744fc9a1cbad4e41b64fb92a52ad822481
Weverton Freitas Perdigão <weverton.perdigao@ITBAM.ORG.BR>
2018-04-03 11:17:35 -0400

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,11 +8,11 @@ import {FuncionariosComponent} from './funcionarios/funcionarios.component';
 import {LoginComponent} from './login/login.component';
 
 export const ROUTES: Routes = [
-  {path: '', component: HomeComponent, canActivate: [LoggedInGuard]},
+  {path: '', component: HomeComponent,}, //canActivate: [LoggedInGuard]
   {path: 'login', component: LoginComponent},
   {path: 'login/:to', component: LoginComponent},
-  {path: 'projetos', component: ProjetosComponent, canActivate: [LoggedInGuard]},
-  {path: 'funcionarios', component: FuncionariosComponent, canActivate: [LoggedInGuard]},
+  {path: 'projetos', component: ProjetosComponent},
+  {path: 'funcionarios', component: FuncionariosComponent},
   {path: 'projeto-detail/:id', component: ProjetoDetailComponent},
   {path: '**', component: NotFoundComponent}
 ];
